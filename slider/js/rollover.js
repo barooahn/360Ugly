d10535311db10c15921019e70a2e80f3b71eda91
john Jones <john Jones>
2016-12-07 16:07:03 +0700

    modal fix and icons for how-works

--- slider/js/rollover.js
+++ slider/js/rollover.js
@@ -5,40 +5,55 @@ load file
 open modal
 start animation
 */
-
-var framesArray = { 
-    elephant: { path: 'slider/images/elephant/DSC_{frame}.jpg', start: 407, end: 436, digits: 4 }, 
-    flowers: { path: 'slider/images/flowers/DSC_{frame}.jpg', start: 457, end: 485, digits: 4 },
-    fireTruck: { path: 'slider/images/fireTruck/DSC_{frame}.jpg', start: 507, end: 536, digits: 4 },
+var framesArray = {
+    elephant: {
+        path: 'slider/images/elephant/DSC_{frame}.jpg',
+        start: 407,
+        end: 436,
+        digits: 4
+    },
+    flowers: {
+        path: 'slider/images/flowers/DSC_{frame}.jpg',
+        start: 457,
+        end: 485,
+        digits: 4
+    },
+    fireTruck: {
+        path: 'slider/images/fireTruck/DSC_{frame}.jpg',
+        start: 507,
+        end: 536,
+        digits: 4
+    },
 }
-
-
 $(".rollover-modal").click(function(e) {
-	e.preventDefault();
+    e.preventDefault();
 
-	var id360 = $(this).attr("id");
-	console.log("ID of rollover = " + id360);	
+    var width = $("#360Modal").width() - 20;
+    var id360 = $(this).attr("id");
+    console.log("ID of rollover = " + id360);
     $(".modal-spin").addClass(id360);
-
     // use helper method to generate an array of image urls. We have 34 frames in total
-	var frames = SpriteSpin.sourceArray(framesArray[id360]['path'], {
-	    frame: [framesArray[id360]['start'], framesArray[id360]['end']],
-	    digits: framesArray[id360]['digits']
-	});
-
-	var spin = $('.'+id360);
-	// initialise spritespin
-	spin.spritespin({
-	    source: frames,
-	    width: 500,
-	    sense: 2,
-	    height: 334,
-	    sizeMode: 'fit',
-	    scrollThreshold: 3000,
-	    frameTime: 100, // Time in ms between updates. 40 is exactly 25 FPS
-	    detectSubsampling: false,
-	    animate: true
-	});
+    var frames = SpriteSpin.sourceArray(framesArray[id360]['path'], {
+        frame: [framesArray[id360]['start'], framesArray[id360]['end']],
+        digits: framesArray[id360]['digits']
+    });
+    var spin = $('.' + id360);
+    // initialise spritespin
+    spin.spritespin({
+        source: frames,
+        width: width,
+        height: 334,
+        sense: 2,
+        sizeMode: 'fit',
+        scrollThreshold: 3000,
+        frameTime: 100, // Time in ms between updates. 40 is exactly 25 FPS
+        detectSubsampling: false,
+        animate: true,
+    });
 
     $("#360Modal").modal();
+    $('#360Modal').on('hidden.bs.modal', function() {
+        spin.spritespin("destroy");
+        console.log('destroyed');
+    });
 });
