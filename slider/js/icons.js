610dbd7e9578efc70e5bb00fa3b9536b17ad091d
john Jones <john Jones>
2016-12-16 13:39:58 +0700

    Added popovers

---
+++ slider/js/icons.js
@@ -0,0 +1,11 @@
+
+$(function () {
+  $('[data-toggle="popover"]').popover({ trigger: "hover click focus" });
+})
+
+// $(".icons").mouseover(function() {
+// 	console.log('ére ' + this);
+//     $(this +" a").popover('show');
+// }).mouseout(function() {
+//     $(this+" a").popover('hide');
+// });
