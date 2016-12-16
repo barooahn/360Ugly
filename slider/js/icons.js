625c53aad601ee632207909f127a0032f3c5251a
john Jones <john Jones>
2016-12-16 13:48:41 +0700

    Added popovers - mobile

--- slider/js/icons.js
+++ slider/js/icons.js
@@ -1,6 +1,7 @@
 
+var is_touch_device = ("ontouchstart" in window) || window.DocumentTouch && document instanceof DocumentTouch;
 $(function () {
-  $('[data-toggle="popover"]').popover({ trigger: "hover click focus" });
+  $('[data-toggle="popover"]').popover({ trigger: is_touch_device ? "click hover" : "hover" });
 })
 
 // $(".icons").mouseover(function() {
